98931d6857bcaea8ab8eac502b17f36e7e13af77
Valeria Jauregui Lorda <facujlorda@alu.ing.unlp.edu.ar>
2026-03-02 17:57:45 +0000

    feat: initial spring boot structure from spring initializr

---
+++ af-crm-server/src/main/java/com/afcrm/server/AfCrmServerApplication.java
@@ -0,0 +1,13 @@
+package com.afcrm.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfCrmServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfCrmServerApplication.class, args);
+	}
+
+}
---
+++ af-crm-server/src/test/java/com/afcrm/server/AfCrmServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afcrm.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfCrmServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
